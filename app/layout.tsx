b9578f7ffdbd3a27efe2b96f277cd12b3e71522a
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-04-18 16:00:44 -0400

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import { Nunito } from 'next/font/google'
+import './globals.css'
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'My Airbnb Clone!',
+}
+
+const font = Nunito({
+  subsets: [
+    "latin"
+  ],
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
