1289283c66f6f1cec121287f73f8a327afda5d26
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-04-23 21:38:01 -0400

    Added CloseButton to Modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useCallback, useEffect, useState } from "react";
+import { IoMdClose } from 'react-icons/io';
 
 interface ModalProps {
     isOpen?: boolean;
@@ -54,12 +55,112 @@ const Modal : React.FC<ModalProps> = ({
     }, [disabled, onSubmit]);
 
     const handleSecondaryAction = useCallback(() => {
+        if (disabled || !secondaryAction) {
+            return;
+        }
 
-    }, []);
+        secondaryAction();
+    }, [disabled, secondaryAction]);
+    
+    if (!isOpen) {
+        return null;
+    }
 
     return (
-        <div></div>
+        <>
+            {/* Dark Overlay */}
+            <div
+                className="
+                    flex
+                    justify-center
+                    items-center
+                    overflow-x-hidden
+                    overflow-y-auto
+                    fixed
+                    inset-0
+                    z-50
+                    outline-none
+                    focus:outline-none
+                    bg-neutral-800/70
+                "
+            >
+                <div
+                    className="
+                        relative
+                        w-full
+                        md:w-4/6
+                        lg:w-3/6
+                        xl:w-2/5
+                        my-6
+                        mx-auto
+                        h-full
+                        lg:h-auto
+                        md:h-auto
+                    "
+                >
+                    {/* Content  */}
+                    <div
+                        className={`
+                            translate
+                            duration-300
+                            h-full
+                            ${showModal ? 'translate-y-0' : 'translate-y-full'}
+                            ${showModal ? 'opacity-100' : 'opacity-0'}
+                        `}
+                    >
+                        <div 
+                            className="
+                                translate
+                                h-full
+                                lg:h-auto
+                                md:h-auto
+                                border-0
+                                rounded-lg
+                                relative
+                                flex
+                                flex-col
+                                w-full
+                                bg-white
+                                outline-none
+                                focus:outline-none
+
+                            "
+                        >
+                            {/* Header */}
+                            <div
+                                className="
+                                    flex
+                                    items-center
+                                    p-6
+                                    rounded-t
+                                    justify-center
+                                    relative
+                                    border-b-[1px]
+                                    
+                                "
+                            >
+                                <button
+                                    onClick={handleClose}
+                                    className="
+                                        p-1
+                                        border-0
+                                        hover:opacity-70
+                                        transition
+                                        absolute
+                                        left-9
+                                    "
+                                >
+                                    <IoMdClose />
+                                </button>
+                                <div></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
+
 }
 
 export default Modal;
--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ClientOnly>
-          <Modal />
+          <Modal isOpen />
           <Navbar />
         </ClientOnly>
         {children}
