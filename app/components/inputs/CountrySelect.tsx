4836d0b14d946118aee1d001584cdcfdde4202fc
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-03 22:30:32 -0400

    Complete - Country Select

--- app/components/inputs/CountrySelect.tsx
+++ app/components/inputs/CountrySelect.tsx
@@ -48,6 +48,20 @@ const CountrySelect : React.FC<CountrySelectProps> = ({
                         </div>
                     </div>
                 )}
+                classNames={{
+                    control: () => 'p-3 border-2',
+                    input: () => 'text-lg',
+                    option: () => 'text-lg'
+                }}
+                theme={theme => ({
+                    ...theme,
+                    borderRadius: 6,
+                    colors: {
+                        ...theme.colors,
+                        primary: 'black',
+                        primary25: '#FFE4E6'
+                    }
+                })}
             />
         </div>
     );
