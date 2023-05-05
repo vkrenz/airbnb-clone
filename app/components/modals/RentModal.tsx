274a799eb9ac6aedfa46b0cee243f392b254798e
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-05 12:08:17 -0400

    Add ImageUpload

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -12,6 +12,7 @@ import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
 import Counter from '../inputs/Counter';
+import ImageUpload from '../inputs/ImageUpload';
 
 enum STEPS {
     CATEGORY = 0,
@@ -160,6 +161,18 @@ const RentModal = () => {
                     value={bathroomCount}
                     onChange={val => setCustomValue('bathroomCount', val)}
                 />
+            </div>
+        )
+    }
+
+    if (step === STEPS.IMAGES) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Add a photo of your place"
+                    subtitle="Show guests what your place looks like!"
+                />
+                <ImageUpload />
             </div>
         )
     }
