57f0c3629235ecde7e1a45eaf5cf18998c2581d9
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-05 11:21:32 -0400

    Add plus/minus buttons

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -11,6 +11,7 @@ import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
 import dynamic from 'next/dynamic';
+import Counter from '../inputs/Counter';
 
 enum STEPS {
     CATEGORY = 0,
@@ -125,6 +126,21 @@ const RentModal = () => {
                     <Map
                         center={location?.latlng}
                     />
+            </div>
+        )
+    }
+
+    if (step === STEPS.INFO) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Share some basics about your place"
+                    subtitle="What amenities do you have?"
+                />
+                <Counter
+                    title="Guests"
+                    subtitle="How many guests do you allow?"
+                />
             </div>
         )
     }
