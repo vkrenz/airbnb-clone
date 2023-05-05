ce957a5a5802ed58211c8d83c6fa6d6c7737851e
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-05 12:36:09 -0400

    Add price comp.

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -211,6 +211,26 @@ const RentModal = () => {
         )
     }
 
+    if (step === STEPS.PRICE) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Now, set your price"
+                    subtitle="How much do you charge per night?"
+                />
+                <Input
+                    id="price"
+                    label="Price"
+                    formatPrice
+                    type="number"
+                    register={register}
+                    errors={errors}
+                    required
+                />
+            </div>
+        )
+    }
+
     return (
         <Modal
             isOpen={rentModal.isOpen}
