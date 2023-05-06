c3e7d6333b62947f322b8426a330bd16695efc82
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-05 23:56:49 -0400

    Update HeartButton.tsx

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
 
 interface HeartButtonProps {
@@ -15,8 +16,27 @@ const HeartButton : React.FC<HeartButtonProps> = ({
     const toggleFavorite = () => {};
 
     return (
-        <div>
-            HeartButton
+        <div
+            onClick={toggleFavorite}
+            className="
+                relative
+                hover:opacity-80
+                transition
+                cursor-pointer
+            "
+        >
+            <AiOutlineHeart
+                size={28}
+                className="
+                    fill-white
+                    absolute
+                    -top-[2px]
+                    -right-[2px]
+                "
+            />
+            <AiFillHeart
+                
+            />
         </div>
     );
 }
