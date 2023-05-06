06924bf800602c8b7eed74bb8bc6664565530415
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-05 23:52:34 -0400

    Update HeartButton.tsx

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,12 +1,19 @@
 'use client'
 
-interface HeartButtonProps {
+import { SafeUser } from "../types";
 
+interface HeartButtonProps {
+    listingId: string;
+    currentUser?: SafeUser | null;
 }
 
 const HeartButton : React.FC<HeartButtonProps> = ({
-
+    listingId,
+    currentUser
 }) => {
+    const hasFavorited = false;
+    const toggleFavorite = () => {};
+
     return (
         <div>
             HeartButton
