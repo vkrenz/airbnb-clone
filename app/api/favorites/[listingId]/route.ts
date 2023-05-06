4f5d315f0a221a760af9351c10967569a6e38a65
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-06 08:11:27 -0400

    Create route.ts

---
+++ app/api/favorites/[listingId]/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import prisma from '@/app/libs/prismadb';
+
+interface Iparams {
+    listingId?: string;
+}
+
+export async function POST(
+    request: Request,
+    { params }: { params: Iparams }
+) {
+    const currentUser = await getCurrentUser();
+
+    if (!currentUser) return NextResponse.error();
+
+    const { listingId } = params;
+
+    if (!listingId || typeof listingId !== "string") throw new Error('Invalid ID');
+
+    let favoriteIds = [...(currentUser.favoriteIds || [])];
+
+    favoriteIds.push(listingId);
+}
