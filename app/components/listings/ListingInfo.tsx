b2249f9aac37be9e51db5b721886fccf630dd6b3
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-07 11:27:06 -0400

    Update ListingInfo.tsx

--- app/components/listings/ListingInfo.tsx
+++ app/components/listings/ListingInfo.tsx
@@ -1,9 +1,60 @@
 'use client'
 
-const ListingInfo = () => {
+import { IconType } from "react-icons";
+
+import useCountries from "@/app/hooks/useCountries";
+import { SafeUser } from "@/app/types";
+
+import Avatar from "../Avatar";
+
+interface ListingInfoProps {
+    user: SafeUser | null;
+    description: string;
+    guestCount: number;
+    roomCount: number;
+    bathroomCount: number;
+    category: {
+        icon: IconType;
+        label: string;
+        description: string;
+    } | undefined;
+    locationValue: string;
+}
+
+const ListingInfo : React.FC<ListingInfoProps> = ({
+    user,
+    description,
+    guestCount,
+    roomCount,
+    bathroomCount,
+    category,
+    locationValue
+}) => {
+    const { getByValue } = useCountries();
+
+    const coordinates = getByValue(locationValue)?.latlng;
+
     return (
-        <div>
-            Listing Info
+        <div className="col-span-4 flex-col gap-8">
+            <div className="flex flex-col gap-2">
+                <div
+                    className="
+                        text-xl
+                        font-semibold
+                        flex
+                        flex-row
+                        items-center
+                        gap-2
+                    "
+                >
+                    <div>
+                        Hosted by {user?.name}
+                    </div>
+                    <Avatar
+                        src={user?.image}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
