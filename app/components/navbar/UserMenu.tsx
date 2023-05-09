d6466f9ade4ce06d8bf113e08f94dbc0c918c889
✧Victor✧ <68920741+vkrenz@users.noreply.github.com>
2023-05-09 12:27:40 -0400

    Update UserMenu.tsx

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -10,8 +10,7 @@ import useLoginModal from '@/app/hooks/useLoginModal';
 import useRentModal from '@/app/hooks/useRentModal';
 import { signOut } from 'next-auth/react'
 import { SafeUser } from '@/app/types';
-import RentModal from '../modals/RentModal';
-
+import { useRouter } from 'next/navigation';
 interface UserMenuProps {
     currentUser?: SafeUser | null;
 }
@@ -19,6 +18,7 @@ interface UserMenuProps {
 const UserMenu : React.FC<UserMenuProps> = ({
     currentUser
 }) => {
+    const router = useRouter();
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
     const rentModal = useRentModal();
@@ -124,7 +124,7 @@ const UserMenu : React.FC<UserMenuProps> = ({
                         {currentUser ? (
                             <>
                                 <MenuItem
-                                    onClick={() => {}}
+                                    onClick={() => router.push('/trips')}
                                     label="Trips"
                                     isBold
                                 />
